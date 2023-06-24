e285e53abfd88ea80db2c51802d00fafaad700dc
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-24 22:48:21 +0300

    support react svg components

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+            typescript: true,
+            ext: 'tsx',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
