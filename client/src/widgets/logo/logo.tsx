5e169cbbed59249325ec1be216ad786a23126819
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-24 19:38:50 +0300

    logo widget: based on title typography ui, add link

--- client/src/widgets/logo/logo.tsx
+++ client/src/widgets/logo/logo.tsx
@@ -1,10 +1,25 @@
-import { FC } from "react";
-import s from "./logo.module.css";
+'use client';
+
+import { ElementType, FC } from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import s from './logo.module.css';
+import { Title } from '@/shared/ui/typography';
 
 interface Props {
   children?: never;
 }
 
+const title = 'Билетопоиск';
+
 export const Logo: FC<Props> = () => {
-  return <h1 className={s.logo}>Билетопоиск</h1>;
+  const pathname = usePathname();
+  const isMainPage = pathname === '/';
+  const elementType: ElementType = isMainPage ? 'h1' : 'h2';
+
+  return (
+    <Title className={s.logo} as={elementType} weight={700}>
+      {isMainPage ? title : <Link href="/">{title}</Link>}
+    </Title>
+  );
 };
