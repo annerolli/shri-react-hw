d153a10fb9cb06d63944d1853b15ccb1c9b754b7
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-24 15:40:11 +0300

    add footer widget

---
+++ client/src/widgets/footer/footer.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import s from './footer.module.css';
+import clsx from 'clsx';
+
+interface Props {
+  children?: never;
+  className?: string;
+}
+
+export const Footer: FC<Props> = ({ className }) => {
+  return <footer className={clsx(className, s.footer)}>footer</footer>;
+};
