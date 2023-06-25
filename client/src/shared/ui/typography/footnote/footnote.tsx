a9597e38d6ddf137d57c59c2c9c13de05140d0c7
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-25 18:18:03 +0300

    add footnote typography ui

---
+++ client/src/shared/ui/typography/footnote/footnote.tsx
@@ -0,0 +1,18 @@
+import s from './footnote.module.css';
+import clsx from 'clsx';
+import { ElementType, FC } from 'react';
+import { Base, TypographyProps } from '../base';
+
+interface Props extends TypographyProps {
+  as?: Extract<ElementType, 'span' | 'div' | 'p' | 'b' | 'strong' | 'i'>;
+}
+
+export const Footnote: FC<Props> = ({ as: Component = 'p', ...props }) => {
+  return (
+    <Base
+      {...props}
+      as={Component}
+      className={clsx(props.className, s.footnote)}
+    />
+  );
+};
--- client/src/shared/ui/typography/index.ts
+++ client/src/shared/ui/typography/index.ts
@@ -2,3 +2,4 @@ export * from './title';
 export * from './headline';
 export * from './paragraph';
 export * from './subhead';
+export * from './footnote';
