4f163bc4ed7cd2757300d569d7e83009067abb38
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-27 02:47:21 +0300

    wrap movies list item in link

--- client/src/widgets/movies-list/list.tsx
+++ client/src/widgets/movies-list/list.tsx
@@ -1,6 +1,7 @@
 import s from './list.module.css';
 import { MovieCard } from '@/entities/movie';
 import { Movie } from '@/shared/services/api';
+import Link from 'next/link';
 import { FC } from 'react';
 
 interface Props {
@@ -12,7 +13,9 @@ export const List: FC<Props> = ({ data }) => {
     <ul className={s.list}>
       {data.map((movie) => (
         <li key={movie.id}>
-          <MovieCard data={movie} />
+          <Link href={`movies/${movie.id}`}>
+            <MovieCard data={movie} />
+          </Link>
         </li>
       ))}
     </ul>
