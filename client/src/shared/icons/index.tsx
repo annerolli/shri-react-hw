54468cb8330a3b898220301f4fbc50e9964e66e1
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-27 04:11:24 +0300

    add icons

--- client/src/shared/icons/index.tsx
+++ client/src/shared/icons/index.tsx
@@ -1,1 +1,5 @@
 export { default as IconDropDown } from './icon-dropdown.svg';
+export { default as IconBasket } from './icon-basket.svg';
+export { default as IconPlus } from './icon-plus.svg';
+export { default as IconMinus } from './icon-minus.svg';
+export { default as IconClose } from './icon-close.svg';
