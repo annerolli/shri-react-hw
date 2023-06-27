785c00305d742227967e609e6cec342447058cfb
Anastasia Tormozova <aatormozova@gmail.com>
2023-06-27 04:30:38 +0300

    use cart entry into header widget

--- client/src/widgets/header/header.tsx
+++ client/src/widgets/header/header.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Logo } from '@/widgets/logo';
 import s from './header.module.css';
 import clsx from 'clsx';
+import { CartEntry } from '../cart-entry';
 
 interface Props {
   children?: never;
@@ -12,6 +13,7 @@ export const Header: FC<Props> = ({ className }) => {
   return (
     <header className={clsx(className, s.header)}>
       <Logo />
+      <CartEntry />
     </header>
   );
 };
